5508abf7409c5b3e3f50b66f910d42e27054fb64
Audin <sa6yrov@github.com>
2019-11-29 16:01:56 +0600

    deleted hw30 & practice

--- HomeWork3011/src/com/company/Koala.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Koala extends ClimbingAnimals implements Climbeable {
-    public Koala(String name, int age, double weight) {
-        super(name, age, weight);
-    }
-}
