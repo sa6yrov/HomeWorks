bd7b8a9bd17d8ab83f4a7ee5897183eb32de798a
sa6yrov <sabyrov.aidin@gmail.com>
2020-01-14 13:09:14 +0600

    first commit

---
+++ HomeWork44/src/com/company/City.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Objects;
+
+public class City {
+    private Integer id;
+    private String name;
+    private Integer population;
+
+    public City(Integer id, String name, Integer population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id.equals(city.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+}
